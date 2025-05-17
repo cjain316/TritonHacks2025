38733f3d110ca5345f0f3926ee45b1321f627786
TurtleDev <114009355+cjain316@users.noreply.github.com>
2025-05-17 15:04:53 -0700

    interpolation

--- FireSim/src/Topography.java
+++ FireSim/src/Topography.java
@@ -17,11 +17,35 @@ public class Topography {
     }
 
     public void paint(Graphics g) {
+        // Step 1: Find min and max Z
+        int minZ = Integer.MAX_VALUE;
+        int maxZ = Integer.MIN_VALUE;
+
+        for (Point3D p : this.points) {
+            if (p.z < minZ) minZ = p.z;
+            if (p.z > maxZ) maxZ = p.z;
+        }
+
+        double rangeZ = maxZ - minZ;
+        if (rangeZ == 0) rangeZ = 1; // avoid division by zero
+
+        // Step 2: Render pixel by pixel
         for (int x = 0; x < 1920; x++) {
             for (int y = 0; y < 1200; y++) {
-                g.setColor(new Color((int)(interpolateZ(x, y)), 0, 0));
-                g.fillRect(x,y,1,1);
+                double interpolatedZ = interpolateZ(x, y);
+                double normalized = (interpolatedZ - minZ) / rangeZ;
+
+                int red = (int)(normalized * 255);
+                int green = (int)((1 - normalized) * 255);
+
+                g.setColor(new Color(red, green, 0));
+                g.fillRect(x, y, 1, 1);
             }
+        }
+
+        for (Point3D p: points) {
+            g.setColor(Color.BLACK);
+            g.fillRect(p.x-2,p.y-2,4,4);
         }
     }
 
