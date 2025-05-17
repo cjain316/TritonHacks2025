4bba1b8a5ae3d5b336c5092ec2c7ed156f81b701
TurtleDev <114009355+cjain316@users.noreply.github.com>
2025-05-17 11:38:11 -0700

    hog

--- FireSim/src/Fire.java
+++ FireSim/src/Fire.java
@@ -2,15 +2,25 @@ import java.awt.*;
 import java.util.ArrayList;
 
 public class Fire {
-    private ArrayList<Point> borderPoints;
-    private double ROC;
+    public ArrayList<Point> firePoints;
+    public double ROC;
 
-    public Fire() {
-        borderPoints = new ArrayList<Point>();
+    public Fire(int initx, int inity) {
+        firePoints = new ArrayList<Point>();
         ROC = 1;
+        firePoints.add(new Point(initx, inity));
     }
 
-    public void addPoints(Point p) {
-
+    public void update() {
+        
     }
+
+    public void paint(Graphics g) {
+        g.setColor(new Color(250, 130, 0));
+        for (Point p: firePoints) {
+            g.fillRect(p.x, p.y, 1,  1);
+        }
+    }
+
+
 }
