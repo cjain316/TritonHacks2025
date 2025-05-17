e3f1bc8b9bcb4f8f00dfe4a3b2c376eb1b8f8ff0
TurtleDev <114009355+cjain316@users.noreply.github.com>
2025-05-17 11:07:41 -0700

    piss

--- FireSim/src/Main.java
+++ FireSim/src/Main.java
@@ -1,3 +1,70 @@
-public class Main {
+import javax.swing.*;
+import java.awt.event.*;
 
+public class Main extends JPanel implements KeyListener, ActionListener, MouseListener {
+    public static void main(String[] args) {
+        Main m = new Main();
+    }
+
+    Timer t;
+
+    public Main() {
+        JFrame f = new JFrame("LaFireSim2025");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.add(this);
+        f.addKeyListener(this);
+        f.addMouseListener(this);
+
+        f.setResizable(false);
+        f.setExtendedState(JFrame.MAXIMIZED_BOTH);
+
+        t = new Timer(1, this);
+        t.start();
+        f.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
 }
